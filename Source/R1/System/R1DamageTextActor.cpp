50d99cf6ec39c72e13d3146a141e15bda7fa0a54
kdg0711 <shfkemll@gmail.com>
2026-05-03 15:46:15 +0900

    feat: create pooled damage text actor with widget component

---
+++ Source/R1/System/R1DamageTextActor.cpp
@@ -0,0 +1,49 @@
+#include "System/R1DamageTextActor.h"
+#include "Components/WidgetComponent.h"
+#include "System/R1DamageUISubsystem.h"
+#include "UI/R1DamageTextWidget.h"
+
+AR1DamageTextActor::AR1DamageTextActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	SetRootComponent(RootComp);
+
+	DamageTextWidgetComp = CreateDefaultSubobject<UWidgetComponent>(TEXT("DamageTextWidgetComp"));
+	DamageTextWidgetComp->SetupAttachment(RootComp);
+	DamageTextWidgetComp->SetWidgetSpace(EWidgetSpace::Screen); // Float in 3D but face screen
+	DamageTextWidgetComp->SetDrawSize(FVector2D(250.0f, 100.0f));
+
+	static ConstructorHelpers::FClassFinder<UR1DamageTextWidget> WidgetAsset(TEXT("/Script/UMGEditor.WidgetBlueprint'/Game/UI/System/Combat/WBP_DamageText.WBP_DamageText_C'"));
+	if (WidgetAsset.Succeeded())
+	{
+		DamageTextWidgetComp->SetWidgetClass(WidgetAsset.Class);
+	}
+}
+
+void AR1DamageTextActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (UR1DamageTextWidget* DamageWidget = Cast<UR1DamageTextWidget>(DamageTextWidgetComp->GetUserWidgetObject()))
+	{
+		DamageWidget->OnAnimationFinished.AddDynamic(this, &AR1DamageTextActor::HandleAnimationFinished);
+	}
+}
+
+void AR1DamageTextActor::SetDamageInfo(const FR1DamageInfo& DamageInfo)
+{
+	if (UR1DamageTextWidget* DamageWidget = Cast<UR1DamageTextWidget>(DamageTextWidgetComp->GetUserWidgetObject()))
+	{
+		DamageWidget->SetDamageInfo(DamageInfo);
+	}
+}
+
+void AR1DamageTextActor::HandleAnimationFinished()
+{
+	if (UR1DamageUISubsystem* DamageSS = GetWorld()->GetSubsystem<UR1DamageUISubsystem>())
+	{
+		DamageSS->ReturnActorToPool(this);
+	}
+}
