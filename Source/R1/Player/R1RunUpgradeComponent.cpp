cd85c67d6c8489299247249b91fd25f64a261114
kdg0711 <shfkemll@gmail.com>
2026-05-05 15:24:15 +0900

    feat: implement UR1RunUpgradeComponent logic

---
+++ Source/R1/Player/R1RunUpgradeComponent.cpp
@@ -0,0 +1,150 @@
+#include "Player/R1RunUpgradeComponent.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "GameplayEffect.h"
+#include "DataTable/R1StatUpgradeData.h"
+#include "R1GameplayTags.h"
+#include "R1LogChannels.h"
+
+UR1RunUpgradeComponent::UR1RunUpgradeComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UR1RunUpgradeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UR1RunUpgradeComponent::AddPoints(int32 Amount)
+{
+	AvailablePoints += Amount;
+	OnAvailablePointsChanged.Broadcast(AvailablePoints);
+}
+
+void UR1RunUpgradeComponent::UpgradeStat(FGameplayTag StatTag)
+{
+	if (AvailablePoints <= 0)
+	{
+		UE_LOG(LogR1, Warning, TEXT("UR1RunUpgradeComponent::UpgradeStat: Not enough points to upgrade %s"), *StatTag.ToString());
+		return;
+	}
+
+	AvailablePoints--;
+	InvestmentHistory.FindOrAdd(StatTag)++;
+
+	OnAvailablePointsChanged.Broadcast(AvailablePoints);
+	OnInvestmentHistoryChanged.Broadcast(StatTag, InvestmentHistory[StatTag]);
+
+	ApplyRunUpgradeEffect();
+}
+
+void UR1RunUpgradeComponent::Reset()
+{
+	AvailablePoints = 0;
+	InvestmentHistory.Empty();
+	
+	OnAvailablePointsChanged.Broadcast(AvailablePoints);
+	
+	if (RunUpgradeGEHandle.IsValid())
+	{
+		AActor* Owner = GetOwner();
+		UAbilitySystemComponent* ASC = nullptr;
+		if (IAbilitySystemInterface* ASI = Cast<IAbilitySystemInterface>(Owner))
+		{
+			ASC = ASI->GetAbilitySystemComponent();
+		}
+		else if (Owner)
+		{
+			ASC = Owner->FindComponentByClass<UAbilitySystemComponent>();
+		}
+
+		if (ASC)
+		{
+			ASC->RemoveActiveGameplayEffect(RunUpgradeGEHandle);
+		}
+		RunUpgradeGEHandle = FActiveGameplayEffectHandle();
+	}
+}
+
+int32 UR1RunUpgradeComponent::GetInvestmentCount(FGameplayTag StatTag) const
+{
+	return InvestmentHistory.Contains(StatTag) ? InvestmentHistory[StatTag] : 0;
+}
+
+void UR1RunUpgradeComponent::ApplyRunUpgradeEffect()
+{
+	if (!RunUpgradeGEClass)
+	{
+		UE_LOG(LogR1, Error, TEXT("UR1RunUpgradeComponent::ApplyRunUpgradeEffect: RunUpgradeGEClass is null!"));
+		return;
+	}
+	if (!StatUpgradeDataTable)
+	{
+		UE_LOG(LogR1, Error, TEXT("UR1RunUpgradeComponent::ApplyRunUpgradeEffect: StatUpgradeDataTable is null!"));
+		return;
+	}
+
+	AActor* Owner = GetOwner();
+	UAbilitySystemComponent* ASC = nullptr;
+	if (IAbilitySystemInterface* ASI = Cast<IAbilitySystemInterface>(Owner))
+	{
+		ASC = ASI->GetAbilitySystemComponent();
+	}
+	else if (Owner)
+	{
+		ASC = Owner->FindComponentByClass<UAbilitySystemComponent>();
+	}
+
+	if (!ASC)
+	{
+		UE_LOG(LogR1, Error, TEXT("UR1RunUpgradeComponent::ApplyRunUpgradeEffect: Could not find ASC on owner!"));
+		return;
+	}
+
+	// Remove old effect if it exists
+	if (RunUpgradeGEHandle.IsValid())
+	{
+		ASC->RemoveActiveGameplayEffect(RunUpgradeGEHandle);
+		RunUpgradeGEHandle = FActiveGameplayEffectHandle();
+	}
+
+	FGameplayEffectContextHandle EffectContext = ASC->MakeEffectContext();
+	EffectContext.AddSourceObject(this);
+
+	FGameplayEffectSpecHandle SpecHandle = ASC->MakeOutgoingSpec(RunUpgradeGEClass, 1.0f, EffectContext);
+	if (SpecHandle.IsValid())
+	{
+		static const FString ContextString(TEXT("UR1RunUpgradeComponent::ApplyRunUpgradeEffect"));
+		TArray<FR1StatUpgradeData*> AllRows;
+		StatUpgradeDataTable->GetAllRows<FR1StatUpgradeData>(ContextString, AllRows);
+
+		for (const auto& Pair : InvestmentHistory)
+		{
+			FGameplayTag StatTag = Pair.Key;
+			int32 InvestmentCount = Pair.Value;
+
+			FR1StatUpgradeData* FoundData = nullptr;
+			for (FR1StatUpgradeData* Row : AllRows)
+			{
+				if (Row && Row->StatTag == StatTag)
+				{
+					FoundData = Row;
+					break;
+				}
+			}
+
+			if (FoundData)
+			{
+				float TotalBonus = InvestmentCount * FoundData->IncreaseAmount;
+				SpecHandle.Data.Get()->SetSetByCallerMagnitude(StatTag, TotalBonus);
+			}
+			else
+			{
+				UE_LOG(LogR1, Warning, TEXT("UR1RunUpgradeComponent::ApplyRunUpgradeEffect: Could not find data for tag %s in DataTable"), *StatTag.ToString());
+			}
+		}
+
+		RunUpgradeGEHandle = ASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+	}
+}
---
+++ Source/R1/Player/R1RunUpgradeComponent.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameplayTagContainer.h"
+#include "GameplayEffectTypes.h"
+#include "R1RunUpgradeComponent.generated.h"
+
+class UGameplayEffect;
+class UDataTable;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAvailablePointsChangedSignature, int32, NewPoints);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnInvestmentHistoryChangedSignature, FGameplayTag, StatTag, int32, NewCount);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class R1_API UR1RunUpgradeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UR1RunUpgradeComponent();
+
+	UFUNCTION(BlueprintCallable, Category = "Run Upgrade")
+	void AddPoints(int32 Amount);
+
+	UFUNCTION(BlueprintCallable, Category = "Run Upgrade")
+	void UpgradeStat(FGameplayTag StatTag);
+
+	UFUNCTION(BlueprintCallable, Category = "Run Upgrade")
+	void Reset();
+
+	UFUNCTION(BlueprintPure, Category = "Run Upgrade")
+	int32 GetInvestmentCount(FGameplayTag StatTag) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Run Upgrade")
+	void ApplyRunUpgradeEffect();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Run Upgrade")
+	FOnAvailablePointsChangedSignature OnAvailablePointsChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "Run Upgrade")
+	FOnInvestmentHistoryChangedSignature OnInvestmentHistoryChanged;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Run Upgrade")
+	int32 AvailablePoints = 0;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Run Upgrade")
+	TMap<FGameplayTag, int32> InvestmentHistory;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Run Upgrade")
+	TSubclassOf<UGameplayEffect> RunUpgradeGEClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Run Upgrade")
+	UDataTable* StatUpgradeDataTable;
+
+	UPROPERTY(Transient)
+	FActiveGameplayEffectHandle RunUpgradeGEHandle;
+};
