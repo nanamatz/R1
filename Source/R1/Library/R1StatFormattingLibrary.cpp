01e015b3a1e628053b67ad38ac6783ef2c3f8966
kdg0711 <shfkemll@gmail.com>
2026-05-05 15:36:55 +0900

    feat: add stat formatting and range calculation library

---
+++ Source/R1/Library/R1StatFormattingLibrary.cpp
@@ -0,0 +1,56 @@
+#include "Library/R1StatFormattingLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/Attribute/R1AttributeSet.h"
+#include "AbilitySystem/Attribute/PlayerAttributeSet.h"
+
+FText UR1StatFormattingLibrary::FormatStatValue(float Value, ER1StatDisplayType DisplayType)
+{
+	switch (DisplayType)
+	{
+	case ER1StatDisplayType::Integer:
+		return FText::AsNumber(FMath::FloorToInt(Value));
+	case ER1StatDisplayType::Float:
+	{
+		FNumberFormattingOptions Options;
+		Options.MaximumFractionalDigits = 2;
+		Options.MinimumFractionalDigits = 0;
+		Options.UseGrouping = false;
+		return FText::AsNumber(Value, &Options);
+	}
+	default:
+		return FText::AsNumber(Value);
+	}
+}
+
+FText UR1StatFormattingLibrary::GetFractionText(float Current, float Max)
+{
+	return FText::Format(NSLOCTEXT("R1", "FractionFormat", "{0} / {1}"), 
+		FText::AsNumber(FMath::FloorToInt(Current)), 
+		FText::AsNumber(FMath::FloorToInt(Max)));
+}
+
+FText UR1StatFormattingLibrary::GetWeaponDamageRangeText(UAbilitySystemComponent* ASC)
+{
+	if (!ASC) return FText::GetEmpty();
+
+	bool bFoundBase = false;
+	float BaseDamage = ASC->GetNumericAttribute(UR1AttributeSet::GetBaseDamageAttribute(), bFoundBase);
+	
+	bool bFoundWeapon = false;
+	float WeaponDamage = ASC->GetNumericAttribute(UPlayerAttributeSet::GetWeaponDamageAttribute(), bFoundWeapon);
+	
+	bool bFoundMult = false;
+	float DamageMultiplier = ASC->GetNumericAttribute(UPlayerAttributeSet::GetDamageMultiplierAttribute(), bFoundMult);
+
+	// Default to 1.0 if multiplier not found or is zero
+	if (!bFoundMult || DamageMultiplier == 0.f) DamageMultiplier = 1.0f;
+
+	float TotalBaseValue = (BaseDamage + WeaponDamage) * DamageMultiplier;
+	
+	float Min = TotalBaseValue * 0.75f;
+	float Max = TotalBaseValue * 1.25f;
+
+	return FText::Format(NSLOCTEXT("R1", "DamageRangeFormat", "{0} ~ {1}"), 
+		FText::AsNumber(FMath::FloorToInt(Min)), 
+		FText::AsNumber(FMath::FloorToInt(Max)));
+}
