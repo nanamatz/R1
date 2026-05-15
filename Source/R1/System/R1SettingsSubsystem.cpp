b4d67d2cb78fa11530759b8e58bd31680d0a5291
kdg0711 <shfkemll@gmail.com>
2026-05-15 11:05:48 +0900

    feat: add UR1SettingsSubsystem for settings management

---
+++ Source/R1/System/R1SettingsSubsystem.cpp
@@ -0,0 +1,67 @@
+#include "System/R1SettingsSubsystem.h"
+#include "System/R1SaveGame_Settings.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/GameUserSettings.h"
+
+void UR1SettingsSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+    LoadSettings();
+}
+
+void UR1SettingsSubsystem::ApplySettings()
+{
+    if (UGameUserSettings* UserSettings = UGameUserSettings::GetGameUserSettings())
+    {
+        UserSettings->ApplySettings(false);
+    }
+
+    ApplyAudioSettings();
+    ApplyGameplaySettings();
+    ApplyControlSettings();
+}
+
+void UR1SettingsSubsystem::SaveSettings()
+{
+    if (UGameUserSettings* UserSettings = UGameUserSettings::GetGameUserSettings())
+    {
+        UserSettings->SaveSettings();
+    }
+
+    if (CurrentSettings)
+    {
+        UGameplayStatics::SaveGameToSlot(CurrentSettings, SettingsSaveSlotName, SettingsUserIndex);
+    }
+
+    ApplySettings();
+}
+
+void UR1SettingsSubsystem::LoadSettings()
+{
+    if (UGameplayStatics::DoesSaveGameExist(SettingsSaveSlotName, SettingsUserIndex))
+    {
+        CurrentSettings = Cast<UR1SaveGame_Settings>(UGameplayStatics::LoadGameFromSlot(SettingsSaveSlotName, SettingsUserIndex));
+    }
+
+    if (!CurrentSettings)
+    {
+        CurrentSettings = Cast<UR1SaveGame_Settings>(UGameplayStatics::CreateSaveGameObject(UR1SaveGame_Settings::StaticClass()));
+    }
+
+    ApplySettings();
+}
+
+void UR1SettingsSubsystem::ApplyAudioSettings()
+{
+    // TODO: Implement audio volume setting logic (e.g., via SoundMix or AudioDevice)
+}
+
+void UR1SettingsSubsystem::ApplyGameplaySettings()
+{
+    // TODO: Implement gameplay setting application logic
+}
+
+void UR1SettingsSubsystem::ApplyControlSettings()
+{
+    // TODO: Implement control/accessibility application logic
+}
---
+++ Source/R1/System/R1SettingsSubsystem.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "R1SettingsSubsystem.generated.h"
+
+UCLASS()
+class R1_API UR1SettingsSubsystem : public UGameInstanceSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+    UFUNCTION(BlueprintCallable, Category = "R1|Settings")
+    void ApplySettings();
+
+    UFUNCTION(BlueprintCallable, Category = "R1|Settings")
+    void SaveSettings();
+
+    UFUNCTION(BlueprintCallable, Category = "R1|Settings")
+    void LoadSettings();
+
+    UFUNCTION(BlueprintPure, Category = "R1|Settings")
+    class UR1SaveGame_Settings* GetCustomSettings() const { return CurrentSettings; }
+
+private:
+    UPROPERTY()
+    TObjectPtr<class UR1SaveGame_Settings> CurrentSettings;
+
+    const FString SettingsSaveSlotName = TEXT("Settings");
+    const int32 SettingsUserIndex = 0;
+
+    void ApplyAudioSettings();
+    void ApplyGameplaySettings();
+    void ApplyControlSettings();
+};
