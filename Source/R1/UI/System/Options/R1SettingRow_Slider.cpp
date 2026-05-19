89788e7825a8d95230d3b7a7e065460c6bced7a2
kdg0711 <shfkemll@gmail.com>
2026-05-19 20:01:47 +0900

    feat: add UR1SettingRow_Slider reusable widget base

---
+++ Source/R1/UI/System/Options/R1SettingRow_Slider.cpp
@@ -0,0 +1,54 @@
+#include "UI/System/Options/R1SettingRow_Slider.h"
+#include "Components/TextBlock.h"
+#include "Components/Slider.h"
+
+void UR1SettingRow_Slider::InitSlider(const FText& InOptionName, float MinValue, float MaxValue, float InitialValue)
+{
+    if (Text_OptionName)
+    {
+        Text_OptionName->SetText(InOptionName);
+    }
+
+    if (Slider_Value)
+    {
+        Slider_Value->SetMinValue(MinValue);
+        Slider_Value->SetMaxValue(MaxValue);
+        Slider_Value->SetValue(InitialValue);
+    }
+
+    UpdateDisplay(InitialValue);
+}
+
+void UR1SettingRow_Slider::SetValue(float NewValue)
+{
+    if (Slider_Value)
+    {
+        Slider_Value->SetValue(NewValue);
+    }
+
+    UpdateDisplay(NewValue);
+}
+
+void UR1SettingRow_Slider::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    if (Slider_Value)
+    {
+        Slider_Value->OnValueChanged.AddDynamic(this, &UR1SettingRow_Slider::HandleInternalSliderValueChanged);
+    }
+}
+
+void UR1SettingRow_Slider::HandleInternalSliderValueChanged(float NewValue)
+{
+    UpdateDisplay(NewValue);
+    OnValueChanged.Broadcast(NewValue);
+}
+
+void UR1SettingRow_Slider::UpdateDisplay(float Value)
+{
+    if (Text_ValueDisplay)
+    {
+        Text_ValueDisplay->SetText(FText::FromString(FString::Printf(TEXT("%.0f"), Value)));
+    }
+}
---
+++ Source/R1/UI/System/Options/R1SettingRow_Slider.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/R1UserWidget.h"
+#include "R1SettingRow_Slider.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSliderRowValueChangedSignature, float, NewValue);
+
+UCLASS()
+class R1_API UR1SettingRow_Slider : public UR1UserWidget
+{
+    GENERATED_BODY()
+public:
+    UPROPERTY(BlueprintAssignable, Category = "R1|Events")
+    FOnSliderRowValueChangedSignature OnValueChanged;
+
+    UFUNCTION(BlueprintCallable, Category = "R1|UI")
+    void InitSlider(const FText& InOptionName, float MinValue, float MaxValue, float InitialValue);
+
+    UFUNCTION(BlueprintCallable, Category = "R1|UI")
+    void SetValue(float NewValue);
+
+protected:
+    virtual void NativeConstruct() override;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<class UTextBlock> Text_OptionName;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<class UTextBlock> Text_ValueDisplay;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<class USlider> Slider_Value;
+
+private:
+    UFUNCTION()
+    void HandleInternalSliderValueChanged(float NewValue);
+
+    void UpdateDisplay(float Value);
+};
