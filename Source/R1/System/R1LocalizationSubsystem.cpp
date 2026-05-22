901b3aec6cff7725eed02e6a5455ca759c559c02
kdg0711 <shfkemll@gmail.com>
2026-05-22 13:04:01 +0900

    feat: add UR1LocalizationSubsystem with GetText and OnLanguageChanged delegate

---
+++ Source/R1/System/R1LocalizationSubsystem.cpp
@@ -0,0 +1,43 @@
+#include "System/R1LocalizationSubsystem.h"
+#include "System/R1LocTextRow.h"
+#include "Engine/DataTable.h"
+
+void UR1LocalizationSubsystem::SetLanguage(ER1Language NewLanguage)
+{
+    CurrentLanguage = NewLanguage;
+    OnLanguageChanged.Broadcast();
+}
+
+FText UR1LocalizationSubsystem::GetText(FName Key) const
+{
+    UDataTable* Table = GetTable();
+    if (!Table)
+    {
+        return FText::FromName(Key);
+    }
+
+    const FR1LocTextRow* Row = Table->FindRow<FR1LocTextRow>(Key, TEXT("UR1LocalizationSubsystem::GetText"));
+    if (!Row)
+    {
+        return FText::FromName(Key);
+    }
+
+    return (CurrentLanguage == ER1Language::Korean) ? Row->Korean : Row->English;
+}
+
+UDataTable* UR1LocalizationSubsystem::GetTable() const
+{
+    if (LocalizationTable)
+    {
+        return LocalizationTable;
+    }
+
+    if (!LocalizationTablePath.IsNull())
+    {
+        UR1LocalizationSubsystem* MutableThis = const_cast<UR1LocalizationSubsystem*>(this);
+        MutableThis->LocalizationTable = Cast<UDataTable>(LocalizationTablePath.TryLoad());
+        return MutableThis->LocalizationTable;
+    }
+
+    return nullptr;
+}
---
+++ Source/R1/System/R1LocalizationSubsystem.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "System/R1LanguageTypes.h"
+#include "R1LocalizationSubsystem.generated.h"
+
+DECLARE_MULTICAST_DELEGATE(FOnLanguageChanged);
+
+UCLASS(Config=Game)
+class R1_API UR1LocalizationSubsystem : public UGameInstanceSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    void SetLanguage(ER1Language NewLanguage);
+    ER1Language GetLanguage() const { return CurrentLanguage; }
+
+    FText GetText(FName Key) const;
+
+    FOnLanguageChanged OnLanguageChanged;
+
+private:
+    UPROPERTY(Config)
+    FSoftObjectPath LocalizationTablePath;
+
+    UPROPERTY()
+    TObjectPtr<UDataTable> LocalizationTable;
+
+    ER1Language CurrentLanguage = ER1Language::English;
+
+    UDataTable* GetTable() const;
+};
